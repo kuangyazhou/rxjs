a709be6a3dcb0da4cd7bdcd9f33b14269ae2e012
kuangyazhou <kuangyazhou@tuandai.com>
2018-05-18 18:57:01 +0800

    context

--- src/components/lesson/four.vue
+++ src/components/lesson/four.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-        <span>姓名：{{ name$ }}</span>
-        <span>年龄：{{ age$ }}</span>
-        <button v-stream:click="setName$">点击设置name的值</button>
+    <span>姓名：{{ name$ }}</span>
+    <span>年龄：{{ age$ }}</span>
+    <button v-stream:click="setName$">点击设置name的值</button>
   </div>
 </template>
 
@@ -15,13 +15,25 @@ import VueRx from "vue-rx";
 
 Vue.use(VueRx, Rx);
 export default {
-  domStreams: ["setName$"],
-  subscriptions() {
-    return {
-      age$: Rx.Observable.of(23).map(data => data),
-      name$: this.setName$.map(e => "myName").startWith("")
-    };
-  }
+    domStreams: ["setName$"],
+    subscriptions() {
+        return {
+            age$: Rx.Observable.of(23).map(data => data),
+            name$: this.setName$.map(e => "myName").startWith("")
+        };
+    },
+    methods: {
+        constext() {
+            return new Promise((resolve, reject) => {
+                const { app, router, store } = createApp();
+                router.onReady(() => {});
+
+                Promise.all(matchedComponents.map(components => {}))
+                    .then(() => {})
+                    .catch(reject);
+            }, reject);
+        }
+    }
 };
 </script>
 
