829b49beac5b8d622e65b569af7236f7ccf8cb69
kuangyazhou <kuangyazhou@tuandai.com>
2018-07-11 14:18:10 +0800

    async

---
+++ src/components/async/await.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>async await</div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    name: "await",
+    data() {
+        return {
+            message: ""
+        };
+    },
+    mounted() {
+        this.load();
+    },
+    methods: {
+        async req() {
+            setTimeout(() => {
+                console.log(1);
+                return 1;
+            }, 2000);
+        },
+        async load() {
+            let data = await this.req();
+            console.log(data);
+        }
+    }
+};
+</script>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,44 +1,52 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Todo from '@/components/todo';
-import Lesson from '@/components/lesson/one';
-import Two from '@/components/lesson/two';
-import Three from '@/components/lesson/three';
-import Four from '@/components/lesson/four';
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import Todo from "@/components/todo";
+import Lesson from "@/components/lesson/one";
+import Two from "@/components/lesson/two";
+import Three from "@/components/lesson/three";
+import Four from "@/components/lesson/four";
+import Await from "@/components/async/await";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-    model: history,
-    routes: [{
-            path: '/',
-            name: 'HelloWorld',
-            component: HelloWorld
-        },
-        {
-            path: '/todo',
-            name: 'todo',
-            component: Todo
-        },
-        {
-            path: '/lesson',
-            name: 'lesson',
-            component: Lesson
-        }, {
-            path: '/two',
-            name: 'two',
-            component: Two
-        },
-        {
-            path: '/three',
-            name: 'three',
-            component: Three
-        },
-        {
-            path: '/four',
-            name: 'four',
-            component: Four
-        },
-    ]
-})
+  model: history,
+  routes: [
+    {
+      path: "/",
+      name: "HelloWorld",
+      component: HelloWorld
+    },
+    {
+      path: "/todo",
+      name: "todo",
+      component: Todo
+    },
+    {
+      path: "/lesson",
+      name: "lesson",
+      component: Lesson
+    },
+    {
+      path: "/two",
+      name: "two",
+      component: Two
+    },
+    {
+      path: "/three",
+      name: "three",
+      component: Three
+    },
+    {
+      path: "/four",
+      name: "four",
+      component: Four
+    },
+    {
+      path: "/async",
+      name: "await",
+      component: Await
+    }
+  ]
+});
