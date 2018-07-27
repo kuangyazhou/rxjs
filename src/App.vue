63e7d020657e6ddb9eb7af36f52b6ac98f0cb45e
kuangyazhou <kuangyazhou@tuandai.com>
2018-07-27 18:45:10 +0800

    fuck prototype

--- src/App.vue
+++ src/App.vue
@@ -7,22 +7,40 @@
 
 <script>
 export default {
-  name: 'App',
-  data(){
-    return{
-
+    name: "App",
+    data() {
+        return {};
+    },
+    methods: {
+        fuck() {
+            // 代码1
+            function People() {}
+            var p = new People();
+            p.__proto__ === People.prototype;
+            People.__proto__ === Function.prototype;
+            People.prototype.__proto__ === Object.prototype;
+            //代码2
+            Function.prototype === Function.__proto__;
+            Function.prototype === Object.__proto__;
+            Function.prototype.__proto__ === Object.prototype;
+            Function instanceof Object;
+            //代码3
+            Object instanceof Function;
+            Function instanceof Object;
+            Function instanceof Function;
+            Object instanceof Object;
+        }
     }
-  }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
